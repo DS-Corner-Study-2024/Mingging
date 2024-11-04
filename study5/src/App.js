e7d197c085b93a3220a26d4f39f00b22d788de30
mingging27 <mingging227@gmail.com>
2024-11-04 09:54:08 +0900

    5주차 스터디

---
+++ study5/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
