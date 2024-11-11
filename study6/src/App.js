95dad23671ec11c818a6a2ca52a9b6471a39bbc3
mingging27 <mingging227@gmail.com>
2024-11-11 10:55:05 +0900

    6주차 스터디3

--- study6/src/App.js
+++ study6/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleOnClick() {
+    alert("클릭하셨군요!");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleOnClick}>클릭하세요</button> 
     </div>
   );
 }
