7fb750107966e17ac3f047c62979486ce7d4700d
mingging27 <mingging227@gmail.com>
2024-12-23 17:20:36 +0900

    9주차 사후

--- study7/src/component/Viewer.js
+++ study7/src/component/Viewer.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
+
 const Viewer = ({ count }) => {
-    return (
-      <div>
-        <div>현재 카운트 : </div>
-        <h1>{count}</h1>
-      </div>
-    );
-  };
-  
-  export default Viewer;
-  
+  const [color, setColor] = useState("black"); 
+
+  useEffect(() => {
+    if (count % 2 === 0) {
+      setColor("red");  // 짝수
+    } else {
+      setColor("black");  // 홀수
+    }
+  }, [count]);  // count 값이 변경될 때마다 실행
+
+  return (
+    <div>
+      <div>현재 카운트 : </div>
+      <h1 style={{ color: color }}>{count}</h1>
+    </div>
+  );
+};
+
+export default Viewer;
