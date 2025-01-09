eac38c8f5e2d8f8179266a07978a760f6b2b88c2
mingging27 <mingging227@gmail.com>
2025-01-09 22:28:44 +0900

    11주차 스터디 사후

--- study11/src/App.js
+++ study11/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useRef } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        it.id === action.targetId
+          ? {
+              ...it,
+              isDone: !it.isDone,
+            }
+          : it
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+
+    default:
+      return state;
+  }
+  return state;
+}
 
 function App() {
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current, 
+        content,
+        isDone: false,
+        createData: new Date().getTime(), 
+      },
+    });
+    idRef.current += 1;
+  };
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    })
+  };
+
+  const onDelete = (targetId) => {
+      dispatch({
+        type: "DELETE",
+        targetId,
+      })
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
